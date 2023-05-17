ef9440a2d1ba22b62ea348bd6a067b37731d39c3
JiangYelinGit <897134699@qq.com>
2023-05-17 17:31:47 +0800

    Update _JZ-objc-internal.m

--- JZNavigationExtension/Private/_JZ-objc-internal.m
+++ JZNavigationExtension/Private/_JZ-objc-internal.m
@@ -43,9 +43,9 @@ JZExtensionBarImplementation
     }
 }
 
-- (UIView *)jz_backgroundView {
-    if (@available(iOS 10, *)) {
-        return [self valueForKeyPath:@"_backgroundView._backgroundEffectView"];
+-(UIView *)jz_backgroundView {
+    if (@available(iOS 13, *)) {
+        return self.subviews.firstObject;
     }
     return [self valueForKeyPath:@"_backgroundView"];
 }
